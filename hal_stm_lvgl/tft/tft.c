6aac1586ccd66e83763a1d533c6425f56395bf91
papadkostas <kostas.papadopulos@gmail.com>
2019-12-25 20:21:56 +0200

    Enable DMA2D
    Use DMA2D(ART accelerator) since the issue with artifacts have been found.

--- hal_stm_lvgl/tft/tft.c
+++ hal_stm_lvgl/tft/tft.c
@@ -275,49 +275,39 @@ static void LTDC_Init(void)
 
 static void CopyBuffer(const uint32_t *pSrc, uint32_t *pDst, uint16_t x, uint16_t y, uint16_t xsize, uint16_t ysize)
 {
-	uint32_t row;
-	for(row = y; row < y + ysize; row++ ) {
-		memcpy(&pDst[row * 800 + x], pSrc, xsize * 4);
-		pSrc += xsize;
+	uint32_t destination = (uint32_t)pDst + (y * 800 + x) * 4;
+	uint32_t source      = (uint32_t)pSrc;
+
+	/*##-1- Configure the DMA2D Mode, Color Mode and output offset #############*/
+	hdma2d.Init.Mode         = DMA2D_M2M;
+	hdma2d.Init.ColorMode    = DMA2D_OUTPUT_ARGB8888;
+	hdma2d.Init.OutputOffset = 800 - xsize;
+	hdma2d.Init.AlphaInverted = DMA2D_REGULAR_ALPHA;  /* No Output Alpha Inversion*/
+	hdma2d.Init.RedBlueSwap   = DMA2D_RB_REGULAR;     /* No Output Red & Blue swap */
+
+	/*##-2- DMA2D Callbacks Configuration ######################################*/
+	hdma2d.XferCpltCallback  = NULL;
+
+	/*##-3- Foreground Configuration ###########################################*/
+	hdma2d.LayerCfg[1].AlphaMode = DMA2D_NO_MODIF_ALPHA;
+	hdma2d.LayerCfg[1].InputAlpha = 0xFF;
+	hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_ARGB8888;
+	hdma2d.LayerCfg[1].InputOffset = 0;
+	hdma2d.LayerCfg[1].RedBlueSwap = DMA2D_RB_REGULAR; /* No ForeGround Red/Blue swap */
+	hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA; /* No ForeGround Alpha inversion */
+
+	hdma2d.Instance          = DMA2D;
+
+	/* DMA2D Initialization */
+	if(HAL_DMA2D_Init(&hdma2d) == HAL_OK)
+	{
+		if(HAL_DMA2D_ConfigLayer(&hdma2d, 1) == HAL_OK)
+		{
+			if (HAL_DMA2D_Start(&hdma2d, source, destination, xsize, ysize) == HAL_OK)
+			{
+				/* Polling For DMA transfer */
+				HAL_DMA2D_PollForTransfer(&hdma2d, 100);
+			}
+		}
 	}
-
-	/**********************
-	 * Using DMA2D should be better and faster but makes artifacts on the screen
-	 **********************/
-
-//	uint32_t destination = (uint32_t)pDst + (y * 800 + x) * 4;
-//	uint32_t source      = (uint32_t)pSrc;
-//
-//	/*##-1- Configure the DMA2D Mode, Color Mode and output offset #############*/
-//	hdma2d.Init.Mode         = DMA2D_M2M;
-//	hdma2d.Init.ColorMode    = DMA2D_OUTPUT_ARGB8888;
-//	hdma2d.Init.OutputOffset = 800 - xsize;
-//	hdma2d.Init.AlphaInverted = DMA2D_REGULAR_ALPHA;  /* No Output Alpha Inversion*/
-//	hdma2d.Init.RedBlueSwap   = DMA2D_RB_REGULAR;     /* No Output Red & Blue swap */
-//
-//	/*##-2- DMA2D Callbacks Configuration ######################################*/
-//	hdma2d.XferCpltCallback  = NULL;
-//
-//	/*##-3- Foreground Configuration ###########################################*/
-//	hdma2d.LayerCfg[1].AlphaMode = DMA2D_NO_MODIF_ALPHA;
-//	hdma2d.LayerCfg[1].InputAlpha = 0xFF;
-//	hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_ARGB8888;
-//	hdma2d.LayerCfg[1].InputOffset = 0;
-//	hdma2d.LayerCfg[1].RedBlueSwap = DMA2D_RB_REGULAR; /* No ForeGround Red/Blue swap */
-//	hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA; /* No ForeGround Alpha inversion */
-//
-//	hdma2d.Instance          = DMA2D;
-//
-//	/* DMA2D Initialization */
-//	if(HAL_DMA2D_Init(&hdma2d) == HAL_OK)
-//	{
-//		if(HAL_DMA2D_ConfigLayer(&hdma2d, 1) == HAL_OK)
-//		{
-//			if (HAL_DMA2D_Start(&hdma2d, source, destination, xsize, ysize) == HAL_OK)
-//			{
-//				/* Polling For DMA transfer */
-//				HAL_DMA2D_PollForTransfer(&hdma2d, 100);
-//			}
-//		}
-//	}
 }
