92a289077e6b9affb2ff3c493ae3e077a7cda742
Konstantinos <kostas.papadopulos@gmail.com>
2021-06-06 18:17:26 +0300

    Release V8

--- hal_stm_lvgl/tft/tft.c
+++ hal_stm_lvgl/tft/tft.c
@@ -66,14 +66,6 @@ static void DMA_Config(void);
 static void DMA_TransferComplete(DMA_HandleTypeDef *han);
 static void DMA_TransferError(DMA_HandleTypeDef *han);
 
-#if LV_USE_GPU == 1
-static void MPU_SDRAM_Config(void);
-
-/*Chrom-ART Accelerator functions*/
-static void gpu_fill_cb(lv_disp_drv_t *drv, lv_color_t *dest_buf, const lv_coord_t dest_width, const lv_area_t *fill_area, lv_color_t color);
-static void gpu_blend_cb(lv_disp_drv_t *disp_drv, lv_color_t *dest, const lv_color_t *src, uint32_t length, lv_opa_t opa);
-#endif
-
 /**********************
  *  STATIC VARIABLES
  **********************/
@@ -149,7 +141,6 @@ void monitor_cb(lv_disp_drv_t * d, uint32_t t, uint32_t p)
 void tft_init(void)
 {
 	BSP_SDRAM_Init();
-
 	/* Deactivate speculative/cache access to first FMC Bank to save FMC bandwidth */
 	FMC_Bank1->BTCR[0] = 0x000030D2;
 	LCD_Config();
@@ -163,19 +154,17 @@ void tft_init(void)
 
 	DMA_Config();
 
-	static lv_color_t buf[TFT_HOR_RES * 48];
-	static lv_color_t buf2[TFT_HOR_RES * 48];
-	static lv_disp_buf_t disp_buf;
-	lv_disp_buf_init(&disp_buf, buf, buf2, TFT_HOR_RES * 48);
+	static lv_color_t disp_buf1[TFT_HOR_RES * 48];
+	static lv_color_t disp_buf2[TFT_HOR_RES * 48];
+	static lv_disp_draw_buf_t buf;
+	lv_disp_draw_buf_init(&buf, disp_buf1, disp_buf2, TFT_HOR_RES * 48);
 
 	lv_disp_drv_init(&disp_drv);
+	disp_drv.draw_buf = &buf;
 	disp_drv.flush_cb = tft_flush_cb;
 	disp_drv.monitor_cb = monitor_cb;
-	disp_drv.buffer = &disp_buf;
-#if LV_USE_GPU == 1
-	disp_drv.gpu_fill_cb = gpu_fill_cb;
-	disp_drv.gpu_blend_cb = NULL;
-#endif
+	disp_drv.hor_res = 800;
+	disp_drv.ver_res = 480;
 	lv_disp_drv_register(&disp_drv);
 }
 
@@ -211,141 +200,6 @@ static void tft_flush_cb(lv_disp_drv_t * drv, const lv_area_t * area, lv_color_t
 		while(1);	/*Halt on error*/
 	}
 }
-
-static void CopyBuffer(const uint32_t *pSrc, uint32_t *pDst, uint16_t x, uint16_t y, uint16_t xsize, uint16_t ysize)
-{
-#if LV_USE_GPU == 0
-	uint32_t row;
-	for(row = y; row < y + ysize; row++ ) {
-		memcpy(&pDst[row * 800 + x], pSrc, xsize * 4);
-		pSrc += xsize;
-	}
-#else
-	uint32_t destination = (uint32_t)pDst + (y * TFT_HOR_RES + x) * 4;
-	uint32_t source      = (uint32_t)pSrc;
-
-	/*##-1- Configure the DMA2D Mode, Color Mode and output offset #############*/
-	hdma2d.Init.Mode         = DMA2D_M2M;
-	hdma2d.Init.ColorMode    = DMA2D_OUTPUT_ARGB8888;
-	hdma2d.Init.OutputOffset = TFT_HOR_RES - xsize;
-	hdma2d.Init.AlphaInverted = DMA2D_REGULAR_ALPHA;  /* No Output Alpha Inversion*/
-	hdma2d.Init.RedBlueSwap   = DMA2D_RB_REGULAR;     /* No Output Red & Blue swap */
-
-	/*##-2- DMA2D Callbacks Configuration ######################################*/
-	hdma2d.XferCpltCallback  = NULL;
-
-	/*##-3- Foreground Configuration ###########################################*/
-	hdma2d.LayerCfg[1].AlphaMode = DMA2D_NO_MODIF_ALPHA;
-	hdma2d.LayerCfg[1].InputAlpha = DMA2D_NO_MODIF_ALPHA;
-	hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_ARGB8888;
-	hdma2d.LayerCfg[1].InputOffset = 0;
-	hdma2d.LayerCfg[1].RedBlueSwap = DMA2D_RB_REGULAR; /* No ForeGround Red/Blue swap */
-	hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA; /* No ForeGround Alpha inversion */
-
-	hdma2d.Instance          = DMA2D;
-
-	/* DMA2D Initialization */
-	if(HAL_DMA2D_Init(&hdma2d) == HAL_OK)
-	{
-		if(HAL_DMA2D_ConfigLayer(&hdma2d, 1) == HAL_OK)
-		{
-			if (HAL_DMA2D_BlendingStart(&hdma2d, source, destination, destination, xsize, ysize) == HAL_OK)
-			{
-				/* Polling For DMA transfer */
-				HAL_DMA2D_PollForTransfer(&hdma2d, 10);
-			}
-		}
-	}
-#endif
-}
-
-#if LV_USE_GPU == 1
-
-static void gpu_fill_cb(lv_disp_drv_t *drv, lv_color_t *dest_buf, const lv_coord_t dest_width, const lv_area_t *fill_area, lv_color_t color)
-{
-	SCB_CleanInvalidateDCache();
-
-	uint32_t width = fill_area->x2 - fill_area->x1 + 1;
-	lv_color_t * destination = dest_buf + (dest_width * fill_area->y1 + fill_area->x1);
-
-	hdma2d.Instance = DMA2D;
-	hdma2d.Init.Mode = DMA2D_R2M;
-	hdma2d.Init.ColorMode = DMA2D_OUTPUT_ARGB8888;
-	hdma2d.Init.OutputOffset = dest_width - width;
-	hdma2d.LayerCfg[1].InputAlpha = DMA2D_NO_MODIF_ALPHA;
-	hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_ARGB8888;
-	hdma2d.XferCpltCallback = NULL;
-
-	/* DMA2D Initialization */
-	if (HAL_DMA2D_Init(&hdma2d) == HAL_OK) {
-		if (HAL_DMA2D_ConfigLayer(&hdma2d, 1) == HAL_OK) {
-			for(uint32_t line = fill_area->y1; line <= fill_area->y2; line++) {
-				if (HAL_DMA2D_BlendingStart(&hdma2d, lv_color_to32(color), (uint32_t)destination, (uint32_t)destination, width, 1) == HAL_OK) {
-					destination += width;
-					/* Polling For DMA transfer */
-					HAL_DMA2D_PollForTransfer(&hdma2d, 10);
-				}
-			}
-		}
-	}
-}
-
-static void gpu_blend_cb(lv_disp_drv_t *disp_drv, lv_color_t *dest, const lv_color_t *src, uint32_t length, lv_opa_t opa)
-{
-	SCB_CleanInvalidateDCache();
-
-	hdma2d.Instance = DMA2D;
-	hdma2d.Init.Mode = DMA2D_M2M_BLEND;
-	hdma2d.Init.OutputOffset = 0;
-
-	/* Foreground layer */
-	hdma2d.LayerCfg[1].AlphaMode = DMA2D_REPLACE_ALPHA;
-	hdma2d.LayerCfg[1].InputAlpha = opa;
-	hdma2d.LayerCfg[1].InputColorMode = DMA2D_INPUT_ARGB8888;
-	hdma2d.LayerCfg[1].InputOffset = 0;
-	hdma2d.LayerCfg[1].AlphaInverted = DMA2D_REGULAR_ALPHA;
-
-	/* Background layer */
-	hdma2d.LayerCfg[0].AlphaMode = DMA2D_NO_MODIF_ALPHA;
-	hdma2d.LayerCfg[0].InputColorMode = DMA2D_INPUT_ARGB8888;
-	hdma2d.LayerCfg[0].InputOffset = 0;
-
-	/* DMA2D Initialization */
-	if (HAL_DMA2D_Init(&hdma2d) == HAL_OK) {
-		if (HAL_DMA2D_ConfigLayer(&hdma2d, 0) == HAL_OK && HAL_DMA2D_ConfigLayer(&hdma2d, 1) == HAL_OK) {
-			if (HAL_DMA2D_BlendingStart(&hdma2d, (uint32_t) src, (uint32_t) dest, (uint32_t) dest, length, 1) == HAL_OK) {
-				/* Polling for DMA transfer */
-				HAL_DMA2D_PollForTransfer(&hdma2d, 10);
-			}
-		}
-	}
-
-}
-
-static void MPU_SDRAM_Config(void)
-{
-	MPU_Region_InitTypeDef MPU_InitStruct;
-
-	HAL_MPU_Disable();
-	HAL_MPU_Enable(MPU_PRIVILEGED_DEFAULT);
-
-	// Disable caching of the frame buffer region
-    MPU_InitStruct.Enable = MPU_REGION_ENABLE;
-    MPU_InitStruct.BaseAddress = LAYER0_ADDRESS;
-    MPU_InitStruct.Size = MPU_REGION_SIZE_2MB;
-    MPU_InitStruct.AccessPermission = MPU_REGION_NO_ACCESS;
-    MPU_InitStruct.IsBufferable = MPU_ACCESS_NOT_BUFFERABLE;
-    MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
-    MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
-    MPU_InitStruct.Number = MPU_REGION_NUMBER0;
-    MPU_InitStruct.TypeExtField = MPU_TEX_LEVEL0;
-    MPU_InitStruct.SubRegionDisable = 0x00;
-    MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_DISABLE;
-
-	HAL_MPU_ConfigRegion(&MPU_InitStruct);
-}
-
-#endif
 
 static void LCD_Config(void)
 {
@@ -616,7 +470,7 @@ void HAL_DSI_EndOfRefreshCallback(DSI_HandleTypeDef *hdsi)
         __HAL_DSI_WRAPPER_ENABLE(&hdsi_discovery);
 
         LCD_SetUpdateRegion(0);
-        if(disp_drv.buffer)  lv_disp_flush_ready(&disp_drv);
+        if(disp_drv.draw_buf)  lv_disp_flush_ready(&disp_drv);
     }
 }
 #endif
--- hal_stm_lvgl/touchpad/touchpad.c
+++ hal_stm_lvgl/touchpad/touchpad.c
@@ -24,7 +24,7 @@
 /**********************
  *  STATIC PROTOTYPES
  **********************/
-static bool touchpad_read_cb(lv_indev_drv_t * drv, lv_indev_data_t *data);
+static void touchpad_read_cb(lv_indev_drv_t * drv, lv_indev_data_t *data);
 
 /**********************
  *  STATIC VARIABLES
@@ -46,7 +46,7 @@ void touchpad_init(void)
 {
   BSP_TS_Init(TFT_HOR_RES, TFT_VER_RES);
 
-  lv_indev_drv_t indev_drv;
+  static lv_indev_drv_t indev_drv;
   lv_indev_drv_init(&indev_drv);
   indev_drv.read_cb = touchpad_read_cb;
   indev_drv.type = LV_INDEV_TYPE_POINTER;
@@ -57,7 +57,7 @@ void touchpad_init(void)
  *   STATIC FUNCTIONS
  **********************/
 
-static bool touchpad_read_cb(lv_indev_drv_t * drv, lv_indev_data_t *data)
+static void touchpad_read_cb(lv_indev_drv_t * drv, lv_indev_data_t *data)
 {
 	static int16_t last_x = 0;
 	static int16_t last_y = 0;
@@ -73,6 +73,4 @@ static bool touchpad_read_cb(lv_indev_drv_t * drv, lv_indev_data_t *data)
 		data->point.y = last_y;
 		data->state = LV_INDEV_STATE_REL;
 	}
-
-	return false;
 }
